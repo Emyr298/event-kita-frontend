4eccd99f8fbae8401bb59f9a8b34637f68b297e0
Emyr298 <emi.ru298@gmail.com>
2023-02-04 18:33:17 +0700

    fix: url

--- src/common/constants/config.ts
+++ src/common/constants/config.ts
@@ -2,9 +2,9 @@ export const config = {
   maxNotifications: 3,
   notificationTime: 5000,
   apiUrl: {
-    userInformation: 'http://localhost:3001/auth',
-    setProfile: 'http://localhost:3001/auth/set-profile',
-    events: 'http://localhost:3001/events',
-    eventJoin: 'http://localhost:3001/events/join',
+    userInformation: 'https://event-kita-backend-production.up.railway.app/auth',
+    setProfile: 'https://event-kita-backend-production.up.railway.app/auth/set-profile',
+    events: 'https://event-kita-backend-production.up.railway.app/events',
+    eventJoin: 'https://event-kita-backend-production.up.railway.app/events/join',
   },
 };
